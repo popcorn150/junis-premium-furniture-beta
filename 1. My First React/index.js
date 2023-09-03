139ee3e29067468af18bf87772e928572181c13e
Dark <66648063+popcorn150@users.noreply.github.com>
2023-09-03 23:50:32 +0100

    my first react

---
+++ 1. My First React/index.js
@@ -0,0 +1,10 @@
+const name = "David";
+ReactDOM.render(<h1>{name}</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <ul>
+    <li>Loves Banana</li>
+    <li>Has a girlfriend whom he really adores</li>
+    <li>Loves cartoon and animes</li>
+  </ul>,
+  document.getElementById("root")
+);
